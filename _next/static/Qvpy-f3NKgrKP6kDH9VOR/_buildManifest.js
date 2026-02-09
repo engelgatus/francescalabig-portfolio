744b59367da3b3cbed3dac5c1e33df9f91861699
engelgatus <engelgatus@gmail.com>
2026-02-09 18:26:20 +0800

    Updates

--- _next/static/Qvpy-f3NKgrKP6kDH9VOR/_buildManifest.js
+++ _next/static/Qvpy-f3NKgrKP6kDH9VOR/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/francescalabig-portfolio//_next/:path+",
+        "destination": "/francescalabig-portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
